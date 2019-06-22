15f9006a70b87a5512335618bff10792735bae82
AdrianD94 <ady.drozman2007@gmail.com>
2019-06-22 19:50:30 +0300

    Add Register,Login,Auth Middleware

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
+
+//connect database
+connectDb();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send({msg:'test'});
+  res.send({ msg: 'test' });
 });
 
 // Define Routes
 
-app.use('/api/users',require('./routes/users'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/contacts',require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
